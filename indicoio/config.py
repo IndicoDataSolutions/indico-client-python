9f54b9bd1e61eec2497af3416199647e5a0e1e9f
Chris Lee <sihrc.c.lee@gmail.com>
2019-10-18 02:28:49 -0400

    FIX: configuration setting

--- indicoio/config.py
+++ indicoio/config.py
@@ -25,17 +25,18 @@ def resolve_api_token(path=None):
         return f.read().strip()
 
 
-default_config_options = {
-    "host": host,
-    "protocol": url_protocol,
-    "serializer": serializer,
-    "short_lived_access_token": None,
-    "request_session": None,
-    "token_path": None,
-}
-
 class RequestConfigMixin(object):
     def __init__(self, config_options=None):
+
+        default_config_options = {
+            "host": host,
+            "protocol": url_protocol,
+            "serializer": serializer,
+            "short_lived_access_token": None,
+            "request_session": None,
+            "token_path": None,
+        }
+
         config_options = config_options or {}
         for option, value in default_config_options.items():
             if option not in config_options:
