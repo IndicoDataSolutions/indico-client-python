897374aeba978e3fd454f35b026405d6ebd79b97
Dan Mepham <dan.mepham@indico.io>
2020-02-10 10:49:29 -0500

    ADD: update version to 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="indicoio",
-    version="2.0.0",
+    version="2.0.1",
     packages=find_packages(),
     description="""A Python Wrapper for indico app API.""",
     license="MIT License (See LICENSE)",
