f51dce4f10cb07d0984c015c485f9e81b74783e6
Nicolas Kassis <nic@indico.io>
2020-03-08 10:16:16 -0400

    inspect full schema

--- indicoio/graphql/graphql_client.py
+++ indicoio/graphql/graphql_client.py
@@ -23,20 +23,125 @@ class GraphClient(RequestProxy):
             )
         return response
 
-    def inspect_schema(self, type_name):
-        return self.query(
-            f"""query {{
-            __type(name: "{type_name}") {{
-                name
-                fields {{
+    def inspect_schema(self, type_name: str=None):
+        if type_name:
+            return self.query(
+                f"""query {{
+                __type(name: "{type_name}") {{
                     name
-                    type {{
+                    fields {{
                         name
-                        kind
+                        type {{
+                            name
+                            kind
+                        }}
                     }}
                 }}
             }}
-        }}
-        """
-        )
+            """
+            )
+        else:
+            return self.query(
+                """
+                query IntrospectionQuery {
+                    __schema {
+                        queryType {
+                        name
+                        }
+                        mutationType {
+                        name
+                        }
+                        subscriptionType {
+                        name
+                        }
+                        types {
+                        ...FullType
+                        }
+                        directives {
+                        name
+                        description
+                        locations
+                        args {
+                            ...InputValue
+                        }
+                        }
+                    }
+                    }
+
+                    fragment FullType on __Type {
+                    kind
+                    name
+                    description
+                    fields(includeDeprecated: true) {
+                        name
+                        description
+                        args {
+                        ...InputValue
+                        }
+                        type {
+                        ...TypeRef
+                        }
+                        isDeprecated
+                        deprecationReason
+                    }
+                    inputFields {
+                        ...InputValue
+                    }
+                    interfaces {
+                        ...TypeRef
+                    }
+                    enumValues(includeDeprecated: true) {
+                        name
+                        description
+                        isDeprecated
+                        deprecationReason
+                    }
+                    possibleTypes {
+                        ...TypeRef
+                    }
+                    }
+
+                    fragment InputValue on __InputValue {
+                    name
+                    description
+                    type {
+                        ...TypeRef
+                    }
+                    defaultValue
+                    }
+
+                    fragment TypeRef on __Type {
+                    kind
+                    name
+                    ofType {
+                        kind
+                        name
+                        ofType {
+                        kind
+                        name
+                        ofType {
+                            kind
+                            name
+                            ofType {
+                            kind
+                            name
+                            ofType {
+                                kind
+                                name
+                                ofType {
+                                kind
+                                name
+                                ofType {
+                                    kind
+                                    name
+                                }
+                                }
+                            }
+                            }
+                        }
+                        }
+                    }
+                }
+                """
+            )
 
