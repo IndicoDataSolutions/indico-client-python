463363414f2cb3edf80b423d54346b576048624e
Nicolas Kassis <nic@indico.io>
2020-03-17 11:08:12 -0400

    Support json_config as dict or str (#2)

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ Setup for indico apis
 from setuptools import setup, find_packages
 
 setup(
-    name="indico-client-python",
-    version="2.0.2",
+    name="indico-client",
+    version="3.1.0",
     packages=find_packages(),
     description="""A Python Wrapper for indico app API.""",
     license="MIT License (See LICENSE)",
--- tests/integration/queries/test_document.py
+++ tests/integration/queries/test_document.py
@@ -24,3 +24,44 @@ def test_document_extraction(indico):
     assert type(extract) == dict
     assert "pages" in extract
 
+
+def test_document_extraction_with_config(indico):
+    client = IndicoClient()
+    dataset_filepath = str(Path(__file__).parents[1]) + "/data/mock.pdf"
+    
+    jobs = client.call(DocumentExtraction(files=[dataset_filepath], json_config={"preset_config": "simple"}))
+
+    assert len(jobs) == 1
+    job = jobs[0]
+    assert job.id != None
+    job = client.call(JobStatus(id=job.id, wait=True))
+    assert job.status == "SUCCESS"
+    assert job.ready == True
+    assert type(job.result["url"]) == str
+
+    extract = client.call(RetrieveStorageObject(
+        job.result
+    ))
+
+    assert type(extract) == dict
+    assert "pages" in extract
+
+def test_document_extraction_with_string_config(indico):
+    client = IndicoClient()
+    dataset_filepath = str(Path(__file__).parents[1]) + "/data/mock.pdf"
+    
+    jobs = client.call(DocumentExtraction(files=[dataset_filepath], json_config='{"preset_config": "simple"}'))
+
+    assert len(jobs) == 1
+    job = jobs[0]
+    assert job.id != None
+    job = client.call(JobStatus(id=job.id, wait=True))
+    assert job.status == "SUCCESS"
+    assert job.ready == True
+    assert type(job.result["url"]) == str
+
+    extract = client.call(RetrieveStorageObject(
+        job.result
+    ))
+    assert type(extract) == dict
+    assert "pages" in extract
