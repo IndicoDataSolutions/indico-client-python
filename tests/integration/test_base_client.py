81309bac7544cc0ce108ae8c11f009ddb7b25c2a
Nicolas Kassis <nic@indico.io>
2020-03-20 15:09:17 -0400

    Added more thorough test

--- tests/integration/test_base_client.py
+++ tests/integration/test_base_client.py
@@ -8,6 +8,10 @@ def test_http_request(indico):
 
 def test_disable_cookie_domain(indico):
     client = IndicoClient(IndicoConfig(_disable_cookie_domain=True))
+    assert client.config._disable_cookie_domain == True
+    cookies = client._http.request_session.cookies
+    auth_token = next(c for c in cookies if c.name == "auth_token")
+    assert auth_token.domain == ""
     response = client.call(HTTPRequest(method=HTTPMethod.GET, path="/auth/users/details"))
     assert "email" in response
 
