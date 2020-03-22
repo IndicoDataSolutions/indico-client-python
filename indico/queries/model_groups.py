86cf6554d5230b8f8c3fea877b425f698bcf27ca
Don Zereski <don@indico.io>
2020-03-22 08:31:49 -0400

    Added Google-style doc strings and fixed lint warnings

--- indico/queries/model_groups.py
+++ indico/queries/model_groups.py
@@ -7,7 +7,21 @@ from indico.types.model import Model
 from indico.types.jobs import Job
 from indico.errors import IndicoNotFound
 
+
 class GetModelGroup(GraphQLRequest):
+    """
+    Get an object describing a model group
+
+    Args:
+        id (int): model group id to query
+
+    Returns:
+        ModelGroup object
+
+    Raises:
+
+    """
+
     query = """
         query GetModelGroup($id: Int){
             modelGroups(modelGroupIds: [$id]) {
@@ -33,10 +47,24 @@ class GetModelGroup(GraphQLRequest):
         mg = ModelGroup(**super().process_response(response)["modelGroups"]["modelGroups"][0])
         return mg
 
+
 class GetTrainingModelWithProgress(GraphQLRequest):
-    query = """ 
+    """
+    Get progress (percent complete) of a training model group
+
+    Args:
+        id (int): model group id to query
+
+    Returns:
+        Model object with percent_complete field
+
+    Raises:
+
+    """
+
+    query = """
         query ModelGroupProgress($id: Int){
-	        modelGroups(modelGroupIds: [$id]){
+            modelGroups(modelGroupIds: [$id]){
                 modelGroups{
                     models {
                         id
@@ -49,6 +77,7 @@ class GetTrainingModelWithProgress(GraphQLRequest):
             }
         }
     """
+
     def __init__(self, id:int):
         super().__init__(query=self.query, variables={
             "id": id
@@ -60,12 +89,13 @@ class GetTrainingModelWithProgress(GraphQLRequest):
         if len(model_groups) != 1:
             raise IndicoNotFound("Model Group")
         models = model_groups[0]["models"]
-        
+
         last = max(models, key=lambda m: m["id"])
         if not last:
             raise IndicoNotFound("Training Model")
-        
+
         return Model(**last)
+
 
 class _CreateModelGroup(GraphQLRequest):
     query = """
@@ -108,7 +138,21 @@ class _CreateModelGroup(GraphQLRequest):
     def process_response(self, response):
         return ModelGroup(**super().process_response(response)["createModelGroup"])
 
+
 class GetModelGroupSelectedModelStatus(GraphQLRequest):
+    """
+    Get the status string of the selected model for the given model group id
+
+    Args:
+        id (int): model group id to query
+
+    Returns:
+        status (str): CREATED, TRAINING, COMPLETE or FAILED
+
+    Raises:
+
+    """
+
     query = """
         query GetModelGroup($id: Int) {
                 modelGroups(modelGroupIds: [$id]) {
@@ -130,17 +174,34 @@ class GetModelGroupSelectedModelStatus(GraphQLRequest):
 
     def process_response(self, response):
         mg = ModelGroup(**super().process_response(response)["modelGroups"]["modelGroups"][0])
-        return mg.selected_model.status 
+        return mg.selected_model.status
 
 
 class CreateModelGroup(RequestChain):
+    """
+    Create a new model group and train a model
+
+    Args:
+        name (str): Name of the new model group
+        dataset_id (int): id of the dataset that this model group is based upon
+        source_column_id (int): id of the source column to use in training this model group. Usually the id of source text or images.
+        labelset_id (int): id of the labelset (labeled data) to use in training this model group
+        wait (bool): Wait for this model group to finish training. Default is False
+
+    Returns:
+        ModelGroup object
+
+    Raises:
+
+    """
+
     def __init__(
         self,
         name: str,
         dataset_id: int,
         source_column_id: int,
         labelset_id: int,
-        wait: bool=False
+        wait: bool = False
     ):
         self.name = name
         self.dataset_id = dataset_id
@@ -157,7 +218,7 @@ class CreateModelGroup(RequestChain):
         )
         model_group_id = self.previous.id
         if self.wait:
-            req =  GetModelGroupSelectedModelStatus(
+            req = GetModelGroupSelectedModelStatus(
                 id = model_group_id
             )
             yield req
@@ -169,13 +230,28 @@ class CreateModelGroup(RequestChain):
 
 
 class ModelGroupPredict(GraphQLRequest):
-    query= """
+    """
+    Generate predictions from a model group on new data
+
+    Args:
+        model_id= (int): model group id use for predictions
+        data= (List[str]): list of samples to predict
+
+    Returns:
+        Job associated with this model group predict task
+
+    Raises:
+
+    """
+
+    query = """
         mutation ModelGroupPredict($modelId: Int!, $data: [String]) {
             modelPredict(modelId: $modelId, data: $data) {
                 jobId
             }
         }
         """
+
     def __init__(self, model_id: int, data: List[str]):
         super().__init__(query=self.query, variables={
             "modelId": model_id,
@@ -184,5 +260,3 @@ class ModelGroupPredict(GraphQLRequest):
 
     def process_response(self, response):
         return Job(**super().process_response(response)["modelPredict"])
-
-
