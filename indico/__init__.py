95c0e81a1d857bf2f6fc87d216cd919b5d7ff3ec
Nicolas Kassis <nic@indico.io>
2020-03-25 11:27:44 -0400

    Fix status check in job wait to correctly check failure

--- indico/__init__.py
+++ indico/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig()
-Version = version = __version__ = VERSION = "3.1.5"
+Version = version = __version__ = VERSION = "3.1.6"
 
 from indico.client import *
 from indico.client.request import *
