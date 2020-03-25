6dc7579013b3e254a3f6b2df20d580426600ca1f
Don Zereski <don@indico.io>
2020-03-25 08:13:01 -0400

    Added Google style doc strings for dataset and datafile types

--- indico/types/datafile.py
+++ indico/types/datafile.py
@@ -1,6 +1,20 @@
 from indico.types.base import BaseType
 
+
 class Datafile(BaseType):
+    """
+    A Datafile in the Indico Platform.
+
+    Datafiles are primarlily used in retrieving stored results.
+
+    Attributes:
+        id (int): The Datafile id
+        name (str): Datafile name
+        deleted (bool): Has the datafile been marked for deletion
+        rainbowUrl (str): URL of the datafile within the Indico Platform
+        fileType (str): Filetype of the datafile
+    """
+
     id: int
     name: str
     deleted: bool
@@ -10,4 +24,3 @@ class Datafile(BaseType):
     fileHash: str
     status: str
     statusMeta: str
-
--- indico/types/dataset.py
+++ indico/types/dataset.py
@@ -3,17 +3,49 @@ from typing import List
 from indico.types.base import BaseType
 from indico.types.datafile import Datafile
 
+
 class DataColumn(BaseType):
+    """
+    A DataColumn in the Indico Platform
+
+    Attributes:
+        id (int): The DataColumn id
+        name (str): DataColumn name
+    """
+
     id: int
     name: str
+
 
 class LabelSet(BaseType):
-    id: int 
+    """
+    A LabelSet in the Indico Platform
+
+    Attributes:
+        id (int): The LabelSet id
+        name (str): LabelSet name
+    """
+
+    id: int
     name: str
 
+
 class Dataset(BaseType):
+    """
+    A Dataset in the Indico Platform
+
+    Attributes:
+        id (int): The Dataset id
+        name (str): Dataset name
+        rowCount (int): Number of rows in the dataset
+        permissions (str): Permissions on the dataset
+        files (List[str]): Names of the file(s) included in the dataset
+        labelsets (List[LabelSet]): LabelSets associated with this dataset
+        datacolumns (List[DataColumn]): DataColumn(s) of the dataset
+    """
+
     id: int
-    name: str   
+    name: str
     rowCount: int
     status: str
     permissions: str
