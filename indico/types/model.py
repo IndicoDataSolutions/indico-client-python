cf41d4c95d2c5d34d51f26f7a3bfb54674784086
Don Zereski <don@indico.io>
2020-03-25 07:23:36 -0400

    Added Google-style doc strings for model group and model types

--- indico/types/model.py
+++ indico/types/model.py
@@ -1,10 +1,31 @@
 from indico.types.base import BaseType
 
+
 class TrainingProgress(BaseType):
+    """
+    Model training progress
+
+    Shows percent complete for a model training
+
+    Attributes:
+        percent_complete (float): Percent complete for a model that's training
+    """
+
     percent_complete: float
 
+
 class Model(BaseType):
+    """
+    A Model in the Indico Platform.
+
+    Models are a part of a Model Group
+
+    Attributes:
+        id (int): The model id. This is different from the model group id.
+        status (str): "CREATED", "TRAINING", "COMPLETE" or "FAILED"
+        training_progress (TrainingProgress): Percent complete training progress on a model.
+    """
+
     id: int
     status: str
     training_progress: TrainingProgress
-
--- indico/types/model_group.py
+++ indico/types/model_group.py
@@ -1,9 +1,24 @@
 from indico.types.base import BaseType
 from indico.types.model import Model
 
+
 class ModelGroup(BaseType):
+    """
+    A Model Group in the Indico Platform.
+
+    Think of Model Groups as a container for individual models that are trained each
+    time you click the Retrain button in Review or label more data in Teach. One of these
+    models will be the current, "selected" model. It's usually the latest model but can also
+    be the best performing.
+
+    Attributes:
+        id (int): The model group id
+        name (str): The name of the model group
+        status (str): "CREATED", "TRAINING", "COMPLETE" or "FAILED"
+        selected model (Model): A Model object representing the selected (active) model.
+    """
+
     id: int
     name: str
     status: str
     selected_model: Model
-
