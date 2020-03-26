31531db82d6510447299ac8e3d887900c9242aa9
Don Zereski <don@indico.io>
2020-03-26 12:48:38 -0400

    Clarified list return from DocumentExtraction

--- examples/single-doc-extraction.py
+++ examples/single-doc-extraction.py
@@ -17,7 +17,7 @@ def main(args):
     if len(args) != 1:
         print('USAGE: doc-extraction.py pdf_dir')
         sys.exit(0)
-    
+
     src_path = Path(args[0])
     if src_path.exists() is False or src_path.is_file() is False:
         print(f'Invalid or non-existing file {src_path}')
@@ -31,8 +31,8 @@ def main(args):
     client = IndicoClient(config=my_config)
 
     # OCR a single file and wait for it to complete
-    job = client.call(DocumentExtraction(files=[src_path], json_config='{"preset_config": "simple"}'))
-    job = client.call(JobStatus(id=job[0].id, wait=True))
+    jobs = client.call(DocumentExtraction(files=[src_path], json_config='{"preset_config": "simple"}'))
+    job = client.call(JobStatus(id=jobs[0].id, wait=True))
     if job is not None and job.status == 'SUCCESS':
         json_data = client.call(RetrieveStorageObject(job.result))
         print(json.dumps(json_data, indent=4))
