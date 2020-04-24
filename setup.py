924476de0d37043306cf48c5e8a95c5e7506d51c
jerry <jerry.genser@indico.io>
2020-04-24 14:56:18 -0400

    FIX:dont require exact python version

--- setup.py
+++ setup.py
@@ -13,10 +13,7 @@ setup(
     url="https://github.com/IndicoDataSolutions/indico-client-python",
     author="indico",
     author_email="engineering@indico.io",
-    tests_require=[
-        "pytest>=5.2.1",
-        "requests-mock>=1.7.0-7"
-    ],
+    tests_require=["pytest>=5.2.1", "requests-mock>=1.7.0-7"],
     install_requires=[
         "msgpack==1.0.0",
         "msgpack-numpy==0.4.4.3",
@@ -24,7 +21,7 @@ setup(
         "Pillow>=6.2.0",
         "requests>=2.22.0",
         "setuptools>=41.4.0",
-        "pandas==0.23.4",
+        "pandas>=0.23.4",
         'importlib-metadata ~= 1.0 ; python_version < "3.8"',
     ],
 )
