534c7e2fa780934480f8f8d519cbb8960925fa8a
Ben Hill <ben.hill@indico.io>
2020-06-15 14:22:58 -0400

    FIX: make _validate an instance method, add message to _validate ValueError

--- indico/filters/base.py
+++ indico/filters/base.py
@@ -19,16 +19,17 @@ class BaseFilter(dict):
         self._validate(filters)
         super().__init__(**filters)
 
+    def _validate(self, keys: Iterable):
+        if not all(key in self._filterable_columns for key in keys):
+            invalid_keys = self._filterable_columns.difference(set(keys))
+            raise ValueError(
+                f"Can only filter on {self._filterable_columns}, not {invalid_keys}"
+            )
+
     @property
     def filterable_columns(self):
         return self._filterable_columns
 
     @classmethod
-    def _validate(cls, keys: Iterable):
-        if not all(key in cls._filterable_columns for key in keys):
-            raise TypeError()
-
-    @classmethod
     def from_dict(cls, filters: Dict[str, Any]):
-        cls._validate(filters.keys())
         return cls(filters)
