b1bfcd31c523a39d04f0854c1728487327be3b03
m c <hello@goat.rocks>
2021-02-24 15:19:50 -0500

    add versioneer files

--- indico/__init__.py
+++ indico/__init__.py
@@ -7,3 +7,7 @@ Version = version = __version__ = VERSION = get_version()
 from indico.client import *
 from indico.client.request import *
 
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
