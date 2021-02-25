35bf3982def31b33e788c3687c7547014fadb62f
m c <hello@goat.rocks>
2021-02-25 12:07:49 -0500

    run tests!

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#put token in user's home dir
+printenv INDICO_API_TOKEN > ~/indico_api_token.txt
 #install additional test reqs and run smoke tests
 pip install requests-mock pytest
 python setup.py install
