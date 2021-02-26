ad0e4887a8c3275f25bfa1a22cb3f97fb75dc60f
m c <hello@goat.rocks>
2021-02-26 15:20:06 -0500

    disable spinner...make tests run correctly w/version update

--- indico/__init__.py
+++ indico/__init__.py
@@ -1,13 +1,8 @@
 import logging
-from ._version import get_version
+from indico._version import get_versions
 
 logging.basicConfig()
-Version = version = __version__ = VERSION = get_version()
+Version = version = __version__ = VERSION = get_versions()['version']
 
 from indico.client import *
 from indico.client.request import *
-
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
