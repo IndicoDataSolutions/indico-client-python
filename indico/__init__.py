88b8dd087f4c889d0a20482a1884dee60176442d
m c <hello@goat.rocks>
2021-03-01 17:26:20 -0500

    fix tagging

--- indico/__init__.py
+++ indico/__init__.py
@@ -6,3 +6,7 @@ Version = version = __version__ = VERSION = get_versions()['version']
 
 from indico.client import *
 from indico.client.request import *
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
