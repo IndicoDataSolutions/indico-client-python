e2e54f4fffad5acd033f6840f43cbdb3aece43f0
m c <hello@goat.rocks>
2021-03-01 09:23:37 -0500

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf /indico-client/dist
-pip install twine
+pip install twine==3.3.0
 python setup.py sdist
 echo $TWINE_USERNAME
 echo $TWINE_REPOSITORY
-twine upload --repository testpypi /indico-client/dist/*
+twine upload /indico-client/dist/*
