8d0f1a1d2fb8466cb9bea59617642585d6ba166f
m c <hello@goat.rocks>
2021-04-23 13:46:02 -0400

    add example

--- examples/get-user-metrics.py
+++ examples/get-user-metrics.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from indico import IndicoConfig, IndicoClient
-from indico
+from indico.queries.usermetrics import GetUserSummary,GetUserSnapshots
+from indico.types.user_metrics import UserSummary
 
 """
 Example 1: User Summary
@@ -9,3 +12,32 @@ my_config = IndicoConfig(
     host="app.indico.io", api_token_path="./path/to/indico_api_token.txt"
 )
 client = IndicoClient(config=my_config)
+
+user_summary: UserSummary = client.call(GetUserSummary())
+print("Wow! there's " + str(user_summary.users.enabled) + " users enabled on the app!")
+print("Did you know there are " + str(len(user_summary.app_roles)) + " roles available here?")
+
+
+"""
+
+Example 2: User Snapshots
+Snapshots are paginated and iterable, 
+so you can continue to iterate over them to build a full set
+"""
+snapshots = []
+for snapshot in client.paginate(GetUserSnapshots(date=datetime.now())):
+    snapshots.extend(snapshot)
+
+print("Fetched " + str(len(snapshots)) + " users for analysis")
+
+"""
+
+Example 3: Filtered User Snapshots
+Filter by userid or email.
+
+"""
+snapshots = []
+for snapshot in client.paginate(GetUserSnapshots(date=datetime.now(), user_id=1)):
+    snapshots.extend(snapshot)
+print("Fetched just " + str(len(snapshots)) + " user for analysis")
+
