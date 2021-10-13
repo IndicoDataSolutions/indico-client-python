b01ac6c6ec7c27fc511782fc18f8909646474551
Adrian LaCour <adrian.lacour@indico.io>
2021-10-13 15:35:49 -0500

    Add the newer file_size and num_pages to SubmissionFile

--- indico/types/submission_file.py
+++ indico/types/submission_file.py
@@ -2,21 +2,24 @@ from indico.types import BaseType
 
 
 class SubmissionFile(BaseType):
-    f"""
+    """
     A Submission File in the Indico Platform.
 
     Submissions files represent a single document, and can be grouped together under
     a single submission to a submission to a workflow.
 
     Attributes:
-        id (int): the Submission file id
+        id (int): The Submission file id
         filepath (str): URL of the input datafile within the Indico Platform.
-        filename (str): name of the original file
-        submission_id (int): the parent Submission id
-
+        filename (str): Name of the original file
+        submission_id (int): The parent Submission id
+        file_size (int): Size of file, in bytes
+        num_pages (int): Number of pages in file
     """
 
     id: int
     filepath: str
     filename: str
     submission_id: int
+    file_size: int
+    num_pages: int
