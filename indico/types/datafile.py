689e124512fe858ae16b4b5cb1b1b9909c76cb16
Adrian LaCour <81830217+adrianlacour-indico@users.noreply.github.com>
2021-11-05 11:24:24 -0500

    [DEV-7866] Add in newer Datafile attributes and the DataFilePage class type (#125)
    * Add in newer Datafile attributes and the DataFilePage class type
    
    * remove errant `s`
    
    * Build update sphinx docs

--- indico/types/datafile.py
+++ indico/types/datafile.py
@@ -1,4 +1,33 @@
+from typing import List
+
 from indico.types.base import BaseType
+
+
+class DataFilePage(BaseType):
+    """
+    A DataFilePage in the Indico Platform
+
+    DataFilePages give more specific info on individual datafile pages.
+
+    Attributes:
+        id (int): The DataFilePage id
+        datafile_id (int): The Datafile id associated with this DataFilePage
+        image (str): URL to where the image of the file is stored
+        page_num (int): The page number of the Datafile associated with this info
+        page_info (str): More meta information on the page, such as OCR stats
+        thumbnail (str): URL to a smaller, thumbnail version of the DataFilePage image
+        doc_start_offset (int): The index position where the page starts
+        doc_end_offset (int): The index position where the page ends
+    """
+
+    id: int
+    datafile_id: int
+    image: str
+    page_num: int
+    page_info: str
+    thumb_nail: str
+    doc_start_offset: int
+    doc_end_offset: int
 
 
 class Datafile(BaseType):
@@ -9,21 +38,30 @@ class Datafile(BaseType):
 
     Attributes:
         id (int): The Datafile id
+        deleted (bool): Has the Datafile been marked for deletion
         name (str): Datafile name
-        deleted (bool): Has the datafile been marked for deletion
-        rainbow_url (str): URL of the datafile within the Indico Platform
-        file_type (str): Filetype of the datafile
-        file_hash (str): Location of the datafile
-        status (str): Processing status of the datafile
+        rainbow_url (str): URL of the Datafile within the Indico Platform
+        status (str): Processing status of the Datafile
+        status_meta (str): Contains metadata about the status of the Datafile, such as errors
+        file_hash (str): Location of the Datafile
+        file_size (int): Size of the file, in bytes
+        file_type (str): Filetype of the Datafile
+        page_ids (List[int]): A list of pagge ids associated with the Datafile
+        num_pages (int): The number of pages in the Datafile
+        pages (List[DataFilePage]): The DatafilePages associated with the Datafile
+        failure_type (str): The type of failure associated with the Datafile, if an error was encountered
     """
 
     id: int
-    name: str
     deleted: bool
-    file_size: int
+    name: str
     rainbow_url: str
-    file_type: str
-    file_hash: str
     status: str
     status_meta: str
+    file_hash: str
+    file_size: int
+    file_type: str
+    page_ids: List[int]
+    num_pages: int
+    pages: List[DataFilePage]
     failure_type: str
