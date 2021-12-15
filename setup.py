34a4236678e07e46dd233a511871be6bba3e8b53
Madison May <madison@indico.io>
2021-12-15 14:53:12 -0500

    Update setup.py (#129)
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="indico-client",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     description="""A Python Wrapper for indico app API.""",
     license="MIT License (See LICENSE)",
     long_description=open("README.rst").read(),
