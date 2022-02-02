f219c9ee425c9935610bbd093139e49cf0931316
Scott <scott.levin@indico.io>
2022-02-02 09:49:10 -0500

    adding two attributes to doc reports

--- tests/integration/queries/test_document_report.py
+++ tests/integration/queries/test_document_report.py
@@ -6,20 +6,29 @@ from indico.queries.document_report import GetDocumentReport
 from indico.types.document_report import DocumentReport
 from indico.filters import DocumentReportFilter
 
+
 def test_fetch_docs(indico):
     client = IndicoClient()
 
     document_report: List[DocumentReport] = client.call(GetDocumentReport())
     assert document_report is not None
     assert len(document_report) > 0
+    assert isinstance(document_report[0].input_files[0].num_pages, int)
+    assert isinstance(document_report[0].input_files[0].file_size, int)
+
 
 def test_fetch_docs_limit(indico):
     client = IndicoClient()
-    filter_opts = DocumentReportFilter(created_at_start_date= datetime(2021,7,1), created_at_end_date = datetime.now())
-    document_report: List[DocumentReport] = client.call(GetDocumentReport(filters=filter_opts, limit=5))
+    filter_opts = DocumentReportFilter(
+        created_at_start_date=datetime(2021, 7, 1), created_at_end_date=datetime.now()
+    )
+    document_report: List[DocumentReport] = client.call(
+        GetDocumentReport(filters=filter_opts, limit=5)
+    )
     assert document_report is not None
     assert len(document_report) > 0
     assert len(document_report) <= 5
+
 
 def test_pagination(indico):
     client = IndicoClient()
