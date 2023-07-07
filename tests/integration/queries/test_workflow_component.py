a95f14a3c42182cccf4400b5b188726a3cf5043c
MC <meg.cahill@indico.io>
2023-07-06 23:11:10 -0400

    improve test for filtered classes

--- tests/integration/queries/test_workflow_component.py
+++ tests/integration/queries/test_workflow_component.py
@@ -1,6 +1,7 @@
-from typing import Tuple
+import jsons
 from indico.client import IndicoClient
 from indico.queries import DeleteWorkflowComponent, GetWorkflow, GetDataset
+from indico.queries.workflow_components import AddLinkClassificationComponent
 from indico.types import ModelGroup, ModelTaskType, NewLabelsetArguments
 
 from ..data.datasets import *  # noqa
@@ -26,7 +27,7 @@ def test_add_mg_new_labelset(indico, org_annotate_dataset):
         source_column_id=source_column_id,
         new_labelset_args=NewLabelsetArguments(
             name=labelset_name,
-            task_type=ModelTaskType.ANNOTATION,
+            task_type=ModelTaskType.CLASSIFICATION_MULTIPLE,
             target_names=["target 1", "target 2"],
             datacolumn_id=source_column_id,
         ),
@@ -54,3 +55,135 @@ def test_delete_workflow_component(
     assert len(wf.components) == num_components - 1
     assert mg_comp_id not in {c.id for c in wf.components}
     assert len(wf.component_links) == num_links - 1
+
+def test_add_many_filtered_classes(indico, org_annotate_dataset):
+    client = IndicoClient()
+    workflowreq = CreateWorkflow(
+        dataset_id=org_annotate_dataset.id, name=f"OrgAnnotate-test-{int(time.time())}"
+    )
+    wf = client.call(workflowreq)
+    mg_name = f"TestAnnotationModelGroup-{int(time.time())}"
+    labelset_name = "test-filtered-classes"
+    after_component_id = next(c.id for c in wf.components if c.component_type == "INPUT_OCR_EXTRACTION")
+    source_column_id = org_annotate_dataset.datacolumn_by_name(
+        "News Headlines w/Company Names"
+    ).id
+    modelgroupreq = AddModelGroupComponent(
+        name=mg_name,
+        dataset_id=org_annotate_dataset.id,
+        workflow_id=wf.id,
+        after_component_id=after_component_id,
+        source_column_id=source_column_id,
+        new_labelset_args=NewLabelsetArguments(
+            name=labelset_name,
+            task_type=ModelTaskType.CLASSIFICATION_MULTIPLE,
+            target_names=["type 1", "type 2", "type 3"],
+            datacolumn_id=source_column_id,
+        ),
+    )
+    wf = client.call(modelgroupreq)
+    mg = next(c for c in wf.components if c.component_type == 'MODEL_GROUP')
+    after_component_id = mg.id
+    
+    classes_to_filter = [
+            ["type 1"],
+            ["type 3"]
+        ]
+    filtered = AddLinkClassificationComponent(
+            workflow_id=wf.id,
+            after_component_id=after_component_id,
+            model_group_id=mg.model_group.id,
+            filtered_classes=classes_to_filter,
+            labels="actual",
+        )
+    wf = client.call(filtered)
+    new_component = next(c.id for c in wf.components if c.component_type == "LINK_CLASSIFICATION_MODEL")
+    assert new_component is not None
+
+def test_add_single_filtered_class(indico, org_annotate_dataset):
+    client = IndicoClient()
+    workflowreq = CreateWorkflow(
+        dataset_id=org_annotate_dataset.id, name=f"OrgAnnotate-test-{int(time.time())}"
+    )
+    wf = client.call(workflowreq)
+    mg_name = f"TestAnnotationModelGroup-{int(time.time())}"
+    labelset_name = "test-filtered-classes"
+    after_component_id = next(c.id for c in wf.components if c.component_type == "INPUT_OCR_EXTRACTION")
+    source_column_id = org_annotate_dataset.datacolumn_by_name(
+        "News Headlines w/Company Names"
+    ).id
+    modelgroupreq = AddModelGroupComponent(
+        name=mg_name,
+        dataset_id=org_annotate_dataset.id,
+        workflow_id=wf.id,
+        after_component_id=after_component_id,
+        source_column_id=source_column_id,
+        new_labelset_args=NewLabelsetArguments(
+            name=labelset_name,
+            task_type=ModelTaskType.CLASSIFICATION_MULTIPLE,
+            target_names=["type 1", "type 2", "type 3"],
+            datacolumn_id=source_column_id,
+        ),
+    )
+    wf = client.call(modelgroupreq)
+    mg = next(c for c in wf.components if c.component_type == 'MODEL_GROUP')
+    after_component_id = mg.id
+    
+    classes_to_filter = [
+            ["type 1"]
+        ]
+    filtered = AddLinkClassificationComponent(
+            workflow_id=wf.id,
+            after_component_id=after_component_id,
+            model_group_id=mg.model_group.id,
+            filtered_classes=classes_to_filter,
+            labels="actual",
+        )
+    wf = client.call(filtered)
+    new_component =  next(c.id for c in wf.components if c.component_type == 'LINK_CLASSIFICATION_MODEL')
+    assert new_component is not None
+
+
+def test_add_bad_syntax_filtered_classes(indico, org_annotate_dataset):
+    client = IndicoClient()
+    workflowreq = CreateWorkflow(
+        dataset_id=org_annotate_dataset.id, name=f"OrgAnnotate-test-{int(time.time())}"
+    )
+    wf = client.call(workflowreq)
+    mg_name = f"TestAnnotationModelGroup-{int(time.time())}"
+    labelset_name = "test-filtered-classes"
+    after_component_id = next(c.id for c in wf.components if c.component_type == "INPUT_OCR_EXTRACTION")
+    source_column_id = org_annotate_dataset.datacolumn_by_name(
+        "News Headlines w/Company Names"
+    ).id
+    modelgroupreq = AddModelGroupComponent(
+        name=mg_name,
+        dataset_id=org_annotate_dataset.id,
+        workflow_id=wf.id,
+        after_component_id=after_component_id,
+        source_column_id=source_column_id,
+        new_labelset_args=NewLabelsetArguments(
+            name=labelset_name,
+            task_type=ModelTaskType.CLASSIFICATION_MULTIPLE,
+            target_names=["type 1", "type 2", "type 3"],
+            datacolumn_id=source_column_id,
+        ),
+    )
+    wf = client.call(modelgroupreq)
+    mg = next(c for c in wf.components if c.component_type == 'MODEL_GROUP')
+    after_component_id = mg.id
+    
+    classes_to_filter = [
+            ["type 1"],
+            ["type 2"]
+        ]
+    filtered = AddLinkClassificationComponent(
+            workflow_id=wf.id,
+            after_component_id=after_component_id,
+            model_group_id=6108,
+            filtered_classes=[classes_to_filter],
+            labels="actual",
+        )
+    with pytest.raises(Exception):
+        wf = client.call(filtered)
+   
