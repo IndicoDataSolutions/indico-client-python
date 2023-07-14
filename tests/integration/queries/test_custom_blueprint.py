4281c2a25cc4f654dfa5b75a2e063c2f8c4f1224
meghanhickey <meghan.hickey@indico.io>
2023-07-14 14:36:15 -0400

    test with bad config

--- tests/integration/queries/test_custom_blueprint.py
+++ tests/integration/queries/test_custom_blueprint.py
@@ -1,5 +1,6 @@
 import pytest
 from indico.client import IndicoClient
+from indico.errors import IndicoInputError
 from indico.queries.custom_blueprint import RegisterCustomBlueprint
 from indico.types.workflow import ComponentFamily
 
@@ -22,3 +23,19 @@ def test_register_blueprint(indico):
     bp = client.call(bpreq)
     print(bp.__dict__)
     assert bp.id
+
+def test_register_blueprint_bad_config(indico):
+    with pytest.raises(IndicoInputError):
+        RegisterCustomBlueprint(
+            component_family=ComponentFamily.OUTPUT, 
+            name="Meowtput", 
+            description="adds 'cat' to the result file", 
+            config={
+                "inputs": [{"name": "result_file", "ioClass": "PartitionStream"}],
+                "outputs": [{"name": "result_file", "ioClass": "PartitionStream"}],
+                "submissionLauncher": {}
+            },
+            tags= ["custom"],
+            all_access=True,
+            footer="version 2"
+        )
