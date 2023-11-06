e867a2c0dfc69004f7afef2a4fdcf7f29140db98
meghanhickey <meghan.hickey@indico.io>
2023-11-06 11:16:59 -0500

    edit doc links so they work in readme; remove uninformationive Raises from docstrings

--- readme_docs/build_docs.sh
+++ readme_docs/build_docs.sh
@@ -3,9 +3,13 @@
 pip install sphinx-markdown-builder==0.6.5 sphinx-autodoc-typehints==1.24.0
 # docs are outputted to ./markdown
 sphinx-build -M markdown ./docsrc .
+cd ./markdown
+
+# update links
+find . -type f -name '*.md' -print0 | xargs -0 sed -i -r 's/\[(`\w+`)\]\(.+\)/\1/'
+find . -type f -name '*.md' -print0 | xargs -0 sed -i 's/.md//'
 
 # move docs into folders for upload
-cd ./markdown
 mkdir types
 mkdir classes
 for filename in $(find . -maxdepth 1 -type f); do
