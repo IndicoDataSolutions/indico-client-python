267a6e78e365fc21d2db63683da7470b0fc94149
thearchitector <elias.gabriel@indicodata.ai>
2024-03-29 16:12:33 -0400

    add: typed type utils

--- indico/types/utils.py
+++ indico/types/utils.py
@@ -1,32 +1,37 @@
 import re
 import time
-from typing import Union
+from typing import TYPE_CHECKING
 
 from indico.errors import IndicoTimeoutError
+
+if TYPE_CHECKING:  # pragma: no cover
+    from typing import NoReturn, Optional, Union
 
 _cc_to_snake_re = re.compile(r"(?<!^)(?=[A-Z])")
 _snake_to_cc_re = re.compile(r"(.*?)_([a-zA-Z])")
 
 
-def cc_to_snake(string: str):
+def cc_to_snake(string: str) -> str:
     return re.sub(_cc_to_snake_re, "_", string).lower()
 
 
-def _camel(match):
+def _camel(match: re.Match[str]) -> str:
     return match.group(1) + match.group(2).upper()
 
 
-def snake_to_cc(string: str):
+def snake_to_cc(string: str) -> str:
     return re.sub(_snake_to_cc_re, _camel, string, 0)
 
 
 class Timer:
-    def __init__(self, timeout: Union[int, float]):
-        self.timeout = timeout
-        self.start = time.time()
-        self.elapsed = 0
+    def __init__(self, timeout: "Union[int, float]"):
+        self.timeout: "Union[int, float]" = timeout
+        self.start: float = time.time()
+        self.elapsed: float = 0
 
-    def check(self):
+    def check(self) -> "Optional[NoReturn]":
         self.elapsed = time.time() - self.start
         if self.timeout < self.elapsed:
             raise IndicoTimeoutError(self.elapsed)
+
+        return None
