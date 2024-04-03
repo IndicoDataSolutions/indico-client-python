f7c2e64eaf1dc49f786c21a4227065ce0c6ba6c5
thearchitector <elias.gabriel@indicodata.ai>
2024-04-03 14:01:09 -0400

    fix: styling

--- indico/client/request.py
+++ indico/client/request.py
@@ -131,7 +131,8 @@ class RequestChain(Generic[RequestClass, FinalResponseType], ABC):
     result: "Optional[FinalResponseType]" = None
 
     @abstractmethod
-    def requests(self) -> "Iterator[RequestClass]": ...
+    def requests(self) -> "Iterator[RequestClass]":
+        ...
 
 
 class Delay:
