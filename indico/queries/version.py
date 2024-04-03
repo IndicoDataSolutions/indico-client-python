2f371e19776efb44e02ecc7b99150d501552e29a
thearchitector <elias.gabriel@indicodata.ai>
2024-04-03 19:39:15 -0400

    fix: client and request generic typing

--- indico/queries/version.py
+++ indico/queries/version.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 from indico.client.request import GraphQLRequest
 
 if TYPE_CHECKING:  # pragma: no cover
-    from indico.typing import AnyDict
+    from indico.typing import Payload
 
 
 class GetIPAVersion(GraphQLRequest[str]):
@@ -13,8 +13,9 @@ class GetIPAVersion(GraphQLRequest[str]):
         }
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(self.query)
 
-    def process_response(self, response: "AnyDict") -> str:
-        return response["ipaVersion"]
+    def process_response(self, response: "Payload") -> str:
+        version: str = super().parse_payload(response)["ipaVersion"]
+        return version
