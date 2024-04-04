5193b021d6332d2632f9ba737f663237d01c35c1
thearchitector <elias.gabriel@indicodata.ai>
2024-04-04 01:55:55 -0400

    fix: 50% typing to queries

--- indico/types/__init__.py
+++ indico/types/__init__.py
@@ -7,7 +7,7 @@ from .model import *
 from .model_export import *
 from .model_group import *
 from .output_file import *
-from .questionnaire import Example  # noqa
+from .questionnaire import Questionnaire  # noqa
 from .submission import *
 from .submission_file import *
 from .workflow import *
