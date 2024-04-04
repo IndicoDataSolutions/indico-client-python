62ba1ff95750deccd4aeeaf5e1416253f4d8e61e
thearchitector <elias.gabriel@indicodata.ai>
2024-04-03 20:46:51 -0400

    fix: types-ing

--- indico/types/__init__.py
+++ indico/types/__init__.py
@@ -1,3 +1,4 @@
+from .base import *
 from .datafile import *
 from .dataset import *
 from .integration import *
@@ -6,7 +7,7 @@ from .model import *
 from .model_export import *
 from .model_group import *
 from .output_file import *
-from .questionnaire import Example
+from .questionnaire import Example  # noqa
 from .submission import *
 from .submission_file import *
 from .workflow import *
