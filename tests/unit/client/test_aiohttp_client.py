392e91f8e9f0f40be1ee6d376b7c00a778186ae0
Jacob Anderson <jacob.anderson0004@gmail.com>
2024-08-13 11:57:52 -0700

    fix tests (#329)

--- tests/unit/client/test_aiohttp_client.py
+++ tests/unit/client/test_aiohttp_client.py
@@ -7,23 +7,17 @@ from indico.client import AIOHTTPClient
 from indico.config import IndicoConfig
 
 
-@pytest.fixture
-async def aiohttp_client():
+@pytest.mark.asyncio
+async def test_handle_files_correct_filename():
     config = IndicoConfig(protocol="https", host="example.com", api_token="dummy_token")
     client = AIOHTTPClient(config=config)
-    yield client
-    await client.request_session.close()
-
-
-@pytest.mark.asyncio
-async def test_handle_files_correct_filename(aiohttp_client):
     file_path = Path("/tmp/testfile.txt")
 
     request_kwargs = {"files": [file_path]}
 
     with patch("pathlib.Path.open", new_callable=MagicMock) as mock_open:
         mock_open.return_value = MagicMock()
-        with aiohttp_client._handle_files(request_kwargs) as file_args:
+        with client._handle_files(request_kwargs) as file_args:
             assert len(file_args) == 1
             for arg in file_args:
                 fields = arg._fields
@@ -31,4 +25,4 @@ async def test_handle_files_correct_filename(aiohttp_client):
                     field_dict = field[0]
                     filename = field_dict.get("filename")
                     assert filename == "testfile.txt"
-    await aiohttp_client.request_session.close()
+    await client.request_session.close()
