f31108c96e203243eb92ff8b116fb8a8ffb17d19
Sung96kim <sung96kim@gmail.com>
2025-07-11 13:46:59 -0400

    fix: other parse

--- indico/client/request.py
+++ indico/client/request.py
@@ -164,19 +164,27 @@ class PagedRequestV2(GraphQLRequest[ResponseType]):
         super().__init__(query, variables=variables)
 
     def parse_payload(
-        self, response: "AnyDict", nested_keys: "Optional[List[str]]" = None
+        self, response: "AnyDict", nested_keys: "Optional[List[str | int]]" = None
     ) -> "Any":
         raw_response: "AnyDict" = cast("AnyDict", super().parse_payload(response))
 
         if nested_keys:
             composite = raw_response
             for key in nested_keys:
-                if key not in composite.keys():
-                    raise IndicoInputError(
-                        f"Nested key not found in response: {key}",
-                    )
-                composite = composite[key]
+                if isinstance(composite, list):
+                    if not isinstance(key, int):
+                        raise IndicoInputError(
+                            f"Invalid nested key type: {type(key)}",
+                        )
+                    composite = composite[int(key)]
+                    continue
 
+                if isinstance(composite, dict):
+                    if key not in composite.keys():
+                        raise IndicoInputError(
+                            f"Nested key not found in response: {key}",
+                        )
+                    composite = composite[key]
             pagination_data = composite
         else:
             pagination_data = next(iter(raw_response.values()))
