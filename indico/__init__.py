a1b64ac346f519f2224ca12dcf4a0fcd910d94f5
Sung96kim <sung96kim@gmail.com>
2025-12-09 16:38:53 -0500

    fix: remove unused type ignore

--- indico/__init__.py
+++ indico/__init__.py
@@ -9,7 +9,7 @@ try:
     __version__ = _get_version_stdlib("indico-client")
 except (ImportError, Exception):
     try:
-        from importlib_metadata import version as _get_version  # type: ignore[reportMissingImports]
+        from importlib_metadata import version as _get_version
 
         __version__ = _get_version("indico-client")
     except (ImportError, Exception):
